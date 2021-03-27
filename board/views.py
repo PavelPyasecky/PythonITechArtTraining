e6c6e08fcb6a62ad0332d169411a6c68ec9f273c
Pavel Piasetcki <pyasecky2010pavel@mail.ru>
2021-03-27 21:42:06 +0300

    Application board has been added

--- board/views.py
+++ board/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, 'board/main.html')
+    games = {1, 2, 3, 4, 5, 6}
+    game_name = "PACMAN"
+    game_desc = "arcade video game"
+    game_img_url = 'static/board/img/pacman.jpg'
+    context = {
+        'games': games,
+        'game_name': game_name,
+        'game_desc': game_desc,
+        'game_img_url': game_img_url,
+    }
+    return render(request, 'board/main.html', context=context)
 
 def detail(request):
     return render(request, 'board/detail.html')
